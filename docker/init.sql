3b21107603d61a0bcd6ebd57dbdacc8a791a08d1
Ilianikonov <nikonov.ilia2013@yandex.ru>
2023-11-19 23:04:41 +0300

    доделал проек и покрыл тестами

--- docker/init.sql
+++ docker/init.sql
@@ -3,35 +3,32 @@ drop table if exists route;
 drop table if exists depo;
 drop table if exists schedule;
 
-create table route_time
+create table schedule
 (
-  id    BIGINT auto_increment primary key,
-  route_id int references route (id),
-  name varchar(50) not null,
-  time_total int,
-  time_obk int,
-  time_flights int
+    id BIGINT auto_increment primary key,
+    date date not null unique
 );
+create table depo
+(
+    id BIGINT auto_increment primary key,
+    schedule_id int references schedule (id),
+    name varchar(500) NOT NULL
 
+);
 create table route
 (
     id       BIGINT auto_increment primary key,
     depo_id int references depo (id),
     number varchar(50) NOT NULL
 );
-
-create table depo
+create table route_time
 (
-  id BIGINT auto_increment primary key,
-  schedule_id int references schedule (id),
-  name varchar(500) NOT NULL
-
-);
-
-create table schedule
-(
-    id BIGINT auto_increment primary key,
-    date date not null unique
+    id    BIGINT auto_increment primary key,
+    route_id int references route (id),
+    name varchar(50) not null,
+    time_total int,
+    time_obk int,
+    time_flights int
 );
 
 create index schedule_date_index on schedule(date);
